31dca0958c29302857dedcb28406b40930a27f2c
golovanevvs <lony@inbox.ru>
2024-08-07 09:19:29 +0300

    Создать iter1

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 	"strings"
@@ -33,12 +34,11 @@ var (
 	metStorage           MemStorage
 )
 
-// main
 func main() {
-	//fmt.Println("Запущен сервер:", addr)
-	//fmt.Println("")
+	fmt.Println("Запущен сервер:", addr)
 	err := http.ListenAndServe(addr, http.HandlerFunc(handlerf))
 	if err != nil {
+		fmt.Println("Ошибка сервера")
 		panic(err)
 	}
 }
@@ -71,63 +71,88 @@ func updateMetrics(m MetricsInt, mT, mN string, mV interface{}) {
 }
 
 func handlerf(w http.ResponseWriter, r *http.Request) {
-	//fmt.Println("")
-	//fmt.Println("Проверка метода")
+	fmt.Println("")
+	fmt.Println("Проверка метода...")
 
 	if r.Method != http.MethodPost {
+		fmt.Println("Недопустимый метод:", r.Method)
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
+	fmt.Println("Метод:", r.Method)
+
+	fmt.Println("")
+	fmt.Println("Проверка Content-Type...")
 	cT := r.Header.Get("Content-Type")
 
 	switch cT {
 	case contentType:
 	default:
-		//fmt.Println("Недопустимый content-type:", cT)
+		fmt.Println("Недопустимый content-type:", cT)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	//fmt.Println("Метод POST")
-	//fmt.Println("")
+	fmt.Println("Проверка Content-Type прошла успешно")
 
 	// Чтение и разделение тела запроса
-	//fmt.Println("Чтение и разделение тела запроса")
+	fmt.Println("")
+	fmt.Println("Чтение и разделение тела запроса...")
 	sbody := strings.Split(r.URL.Path, "/")
 	if len(sbody) != 5 {
-		// 	//fmt.Println("Структура тела запроса не соответствует ожидаемой. Получено тело запроса:", r.URL.Path)
+		fmt.Println("Структура тела запроса не соответствует ожидаемой. Получено тело запроса:", r.URL.Path)
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	//fmt.Println("Получено тело запроса:", r.URL.Path)
-	//fmt.Println("")
+	fmt.Println("Чтение и разделение тела запроса прошло успешно")
+	fmt.Println("Получено тело запроса:", r.URL.Path)
 
-	mM := sbody[1] // Имя метода
-	//fmt.Println("Метод:", mM)
+	fmt.Println("")
+	fmt.Println("Параметры полученной метрики:")
+	mM := sbody[1] // Тип метода
+	fmt.Println("Тип метода:", mM)
 	mT := sbody[2] // Тип метрики
-	//fmt.Println("Тип метрики:", mT)
+	fmt.Println("Тип метрики:", mT)
 	mN := sbody[3] // Имя метрики
-	//fmt.Println("Имя метрики:", mN)
+	fmt.Println("Имя метрики:", mN)
 	mV := sbody[4] // Значение метрики
-	//fmt.Println("Значение метрики:", mV)
-	//fmt.Println("")
+	fmt.Println("Значение метрики:", mV)
+
+	fmt.Println("")
+	fmt.Println("Проверка типа метода...")
 
 	switch mM {
 	case updateMethod:
 	default:
-		//fmt.Println("Неизвестный тип метода:", mM)
+		fmt.Println("Неизвестный тип метода:", mM)
 		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
+
+	fmt.Println("Проверка типа метода прошла успешно")
+
+	fmt.Println("")
+	fmt.Println("Проверка наличия имени метрики...")
+
+	if mN == "" {
+		fmt.Println("Имя метрики не задано")
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	fmt.Println("Проверка наличия имени метрики прошла успешно")
+
+	fmt.Println("")
+	fmt.Println("Проверка значения метрики...")
 
 	switch mT {
 	case gaugeType:
 		mVParse, err := strconv.ParseFloat(mV, 64)
 		if err != nil || mVParse < 0 {
-			// fmt.Println("Значение метрики не соответствует требуемому типу float64:", mV)
-			// fmt.Println(err)
-			// fmt.Println("")
+			fmt.Println("Значение метрики не соответствует требуемому типу float64:", mV)
+			fmt.Println(err)
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
@@ -135,28 +160,35 @@ func handlerf(w http.ResponseWriter, r *http.Request) {
 	case counterType:
 		mVParse, err := strconv.ParseInt(mV, 10, 64)
 		if err != nil || mVParse < 0 {
-			// fmt.Println("Значение метрики не соответствует требуемому типу int64:", mV)
-			// fmt.Println(err)
-			// fmt.Println("")
+			fmt.Println("Значение метрики не соответствует требуемому типу int64:", mV)
+			fmt.Println(err)
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 		updateMetrics(&counterMet, mT, mN, mVParse)
 	default:
-		// fmt.Println("Неизвестный тип метрики")
-		// fmt.Println("")
+		fmt.Println("Неизвестный тип метрики")
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	//fmt.Println("Обновлённая мапа:", metStorage)
-	// fmt.Println("")
-	//fmt.Println("Отправлен статус Ok")
-	// fmt.Println("")
+	fmt.Println("Проверка значения метрики прошла успешно")
+
+	fmt.Println("")
+	fmt.Println("Обновлённая мапа:", metStorage)
+
+	fmt.Println("")
+	fmt.Println("Отправлен Content-Type")
 	w.Header().Set("Content-Type", "text/plain")
+
+	fmt.Println("")
+	fmt.Println("Отправлен код 200")
 	w.WriteHeader(http.StatusOK)
 }
 
 //Для запуска теста iter1
-//metricstest-windows-amd64 -test.v -test.run=^TestIteration1$ -binary-path=C:\dev\projects\yapracticum\metalecoll\cmd\server
 
-//metricstest-windows-amd64 -test.v -test.run=^TestIteration1$ -binary-path=C:\Golovanev\Dev\Projects\YaPracticum\metalecoll\cmd\server
+//surface
+//metricstest-windows-amd64 -test.v -test.run=^TestIteration1$ -binary-path=C:\\dev\\projects\\yapracticum\\metalecoll\\cmd\\server\\server
+
+//home
+//metricstest-windows-amd64 -test.v -test.run=^TestIteration1$ -binary-path=C:\\Golovanev\\Dev\\Projects\\YaPracticum\\metalecoll\\cmd\\server
