dd5445872f7645c15d9204ec7a1d3596ce8e76c6
golovanevvs <lony@inbox.ru>
2024-08-13 23:30:22 +0300

    Что-то

--- internal/server/storage/mapstorage/mapstore.go
+++ internal/server/storage/mapstorage/mapstore.go
@@ -38,18 +38,3 @@ func (ms *memStorage) GetMetrics() map[string]model.Metric {
 func NewStorage() *memStorage {
 	return &memStorage{}
 }
-
-func SM(s Storage, m model.Metric) {
-	s.SaveMetric(m)
-}
-
-func GM(s Storage, key string) (model.Metric, error) {
-	if _, err := s.GetMetric(key); err != nil {
-		return model.Metric{}, err
-	}
-	return s.GetMetric(key)
-}
-
-func GMs(s Storage) map[string]model.Metric {
-	return s.GetMetrics()
-}
