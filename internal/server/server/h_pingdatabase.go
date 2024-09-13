b4f88434c710fc09aa3ea06e1fdc1ff7cf15d0c9
golovanevvs <lony@inbox.ru>
2024-09-13 16:05:58 +0300

    Создать iter10

--- internal/server/server/h_pingdatabase.go
+++ internal/server/server/h_pingdatabase.go
@@ -8,7 +8,7 @@ import (
 )
 
 func PingDatabaseHandler(w http.ResponseWriter, r *http.Request, databaseDNS string) {
-	srv.logger.Debugf("Открытие БД...")
+	srv.logger.Debugf("Открытие БД %v...", databaseDNS)
 	db, err := sql.Open("pgx", databaseDNS)
 	if err != nil {
 		srv.logger.Errorf("Ошибка открытия БД: %v", err)
@@ -18,6 +18,16 @@ func PingDatabaseHandler(w http.ResponseWriter, r *http.Request, databaseDNS str
 	}
 	defer db.Close()
 	srv.logger.Debugf("Открытие БД прошло успешно")
+
+	srv.logger.Debugf("Создание БД...")
+	_, err = db.Exec("CREATE DATABASE metalecoll WITH OWNER = postgres ENCODING = 'UTF8' TABLESPACE = pg_default CONNECTION LIMIT = -1;")
+	if err != nil {
+		srv.logger.Errorf("Ошибка создания БД: %v", err)
+		srv.logger.Errorf("Отправлен код: %v", http.StatusInternalServerError)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	srv.logger.Debugf("Создание БД прошло успешно")
 
 	srv.logger.Debugf("Пингование БД...")
 	if err := db.Ping(); err != nil {
