1983f77268deff55b46539cf23fc74e37d7081fa
golovanevvs <lony@inbox.ru>
2024-10-03 23:34:34 +0300

    Создать iter15

--- internal/agent/agent/regnsave.go
+++ internal/agent/agent/regnsave.go
@@ -2,34 +2,75 @@ package agent
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/golovanevvs/metalecoll/internal/agent/model"
+	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
 )
 
+type reg struct {
+	rtMet runtime.MemStats
+	vm    *mem.VirtualMemoryStat
+	cpuUt []float64
+}
+
+// Реализация Worker Pool
 func regNSave(ag *agent) {
-	const numJobs = 32
+	const numJobs = 32 // количество работ
+
 	var w int
+	var r reg
+	var err error
 
-	w = 3
+	w = 3 // количество воркеров
 
-	fmt.Println("Запуск regNSave")
+	// заполнение полей объекта r
+	runtime.ReadMemStats(&r.rtMet)
 
-	jobs := make(chan int, numJobs)
-	results := make(chan model.Metric, numJobs)
-
-	for i := 1; i <= w; i++ {
-		go regRTMetWorker(jobs, results)
+	r.vm, err = mem.VirtualMemory()
+	if err != nil {
+		r.vm = &mem.VirtualMemoryStat{}
 	}
 
-	for j := 1; j <= numJobs; j++ {
+	r.cpuUt, err = cpu.Percent(0, true)
+	if err != nil {
+		r.cpuUt = []float64{0}
+	}
+
+	// количество работ для вывода результата
+	newNumJobs := numJobs + len(r.cpuUt) - 1
+
+	// создаём буферизованный канал для принятия задач в воркер
+	jobs := make(chan int, numJobs)
+
+	// создаём буферизованный канал для отправки результатов
+	results := make(chan model.Metric, newNumJobs)
+
+	// создаём и запускаем воркеры
+	for i := 0; i < w; i++ {
+		go regRTMetWorker(r, jobs, results)
+	}
+
+	// в канал задач отправляем id задачи
+	for j := 0; j < numJobs; j++ {
 		jobs <- j
 	}
 
-	for a := 1; a <= numJobs; a++ {
+	// забираем из канала результатов результаты
+	for a := 0; a < newNumJobs; a++ {
 		ag.store.SaveMetric(<-results)
 	}
 
+	// закрываем канал на стороне отправителя
 	close(jobs)
-	fmt.Println(ag.store.GetMetricsMap())
-	fmt.Println("Завершение regNSave")
+	close(results)
+
+	// выводим полученное map-хранилище
+	count := 0
+	mapa, _ := ag.store.GetMetricsMap()
+	for _, m := range mapa {
+		count++
+		fmt.Printf("%v. Name: %v, Value:%v\n", count, m.Name, m.Value)
+	}
 }
--- internal/agent/agent/regrtmetgo.go
+++ internal/agent/agent/regrtmetgo.go
@@ -3,94 +3,79 @@ package agent
 import (
 	"fmt"
 	"math/rand/v2"
-	"runtime"
 
 	"github.com/golovanevvs/metalecoll/internal/agent/model"
 	"github.com/golovanevvs/metalecoll/internal/server/constants"
-	"github.com/shirou/gopsutil/cpu"
-	"github.com/shirou/gopsutil/mem"
 )
 
-func regRTMetWorker(jobs <-chan int, results chan<- model.Metric) {
-	var rtMet runtime.MemStats
-
-	vm, err := mem.VirtualMemory()
-	if err != nil {
-		vm = &mem.VirtualMemoryStat{}
-	}
-
-	cpuUt, err := cpu.Percent(0, true)
-	if err != nil {
-		cpuUt = []float64{0}
-	}
-
+func regRTMetWorker(r reg, jobs <-chan int, results chan<- model.Metric) {
 	for j := range jobs {
 		switch j {
 		case 0:
-			results <- model.Metric{Type: constants.GaugeType, Name: "Alloc", Value: float64(rtMet.Alloc)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "Alloc", Value: float64(r.rtMet.Alloc)}
 		case 1:
-			results <- model.Metric{Type: constants.GaugeType, Name: "BuckHashSys", Value: float64(rtMet.BuckHashSys)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "BuckHashSys", Value: float64(r.rtMet.BuckHashSys)}
 		case 2:
-			results <- model.Metric{Type: constants.GaugeType, Name: "Frees", Value: float64(rtMet.Frees)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "Frees", Value: float64(r.rtMet.Frees)}
 		case 3:
-			results <- model.Metric{Type: constants.GaugeType, Name: "GCCPUFraction", Value: float64(rtMet.GCCPUFraction)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "GCCPUFraction", Value: float64(r.rtMet.GCCPUFraction)}
 		case 4:
-			results <- model.Metric{Type: constants.GaugeType, Name: "GCSys", Value: float64(rtMet.GCSys)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "GCSys", Value: float64(r.rtMet.GCSys)}
 		case 5:
-			results <- model.Metric{Type: constants.GaugeType, Name: "HeapAlloc", Value: float64(rtMet.HeapAlloc)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "HeapAlloc", Value: float64(r.rtMet.HeapAlloc)}
 		case 6:
-			results <- model.Metric{Type: constants.GaugeType, Name: "HeapIdle", Value: float64(rtMet.HeapIdle)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "HeapIdle", Value: float64(r.rtMet.HeapIdle)}
 		case 7:
-			results <- model.Metric{Type: constants.GaugeType, Name: "HeapInuse", Value: float64(rtMet.HeapInuse)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "HeapInuse", Value: float64(r.rtMet.HeapInuse)}
 		case 8:
-			results <- model.Metric{Type: constants.GaugeType, Name: "HeapObjects", Value: float64(rtMet.HeapObjects)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "HeapObjects", Value: float64(r.rtMet.HeapObjects)}
 		case 9:
-			results <- model.Metric{Type: constants.GaugeType, Name: "HeapReleased", Value: float64(rtMet.HeapReleased)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "HeapReleased", Value: float64(r.rtMet.HeapReleased)}
 		case 10:
-			results <- model.Metric{Type: constants.GaugeType, Name: "HeapSys", Value: float64(rtMet.HeapSys)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "HeapSys", Value: float64(r.rtMet.HeapSys)}
 		case 11:
-			results <- model.Metric{Type: constants.GaugeType, Name: "LastGC", Value: float64(rtMet.LastGC)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "LastGC", Value: float64(r.rtMet.LastGC)}
 		case 12:
-			results <- model.Metric{Type: constants.GaugeType, Name: "Lookups", Value: float64(rtMet.Lookups)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "Lookups", Value: float64(r.rtMet.Lookups)}
 		case 13:
-			results <- model.Metric{Type: constants.GaugeType, Name: "MCacheInuse", Value: float64(rtMet.MCacheInuse)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "MCacheInuse", Value: float64(r.rtMet.MCacheInuse)}
 		case 14:
-			results <- model.Metric{Type: constants.GaugeType, Name: "MCacheSys", Value: float64(rtMet.MCacheSys)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "MCacheSys", Value: float64(r.rtMet.MCacheSys)}
 		case 15:
-			results <- model.Metric{Type: constants.GaugeType, Name: "MSpanInuse", Value: float64(rtMet.MSpanInuse)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "MSpanInuse", Value: float64(r.rtMet.MSpanInuse)}
 		case 16:
-			results <- model.Metric{Type: constants.GaugeType, Name: "MSpanSys", Value: float64(rtMet.MSpanSys)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "MSpanSys", Value: float64(r.rtMet.MSpanSys)}
 		case 17:
-			results <- model.Metric{Type: constants.GaugeType, Name: "Mallocs", Value: float64(rtMet.Mallocs)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "Mallocs", Value: float64(r.rtMet.Mallocs)}
 		case 18:
-			results <- model.Metric{Type: constants.GaugeType, Name: "NextGC", Value: float64(rtMet.NextGC)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "NextGC", Value: float64(r.rtMet.NextGC)}
 		case 19:
-			results <- model.Metric{Type: constants.GaugeType, Name: "NumForcedGC", Value: float64(rtMet.NumForcedGC)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "NumForcedGC", Value: float64(r.rtMet.NumForcedGC)}
 		case 20:
-			results <- model.Metric{Type: constants.GaugeType, Name: "NumGC", Value: float64(rtMet.NumGC)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "NumGC", Value: float64(r.rtMet.NumGC)}
 		case 21:
-			results <- model.Metric{Type: constants.GaugeType, Name: "OtherSys", Value: float64(rtMet.OtherSys)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "OtherSys", Value: float64(r.rtMet.OtherSys)}
 		case 22:
-			results <- model.Metric{Type: constants.GaugeType, Name: "PauseTotalNs", Value: float64(rtMet.PauseTotalNs)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "PauseTotalNs", Value: float64(r.rtMet.PauseTotalNs)}
 		case 23:
-			results <- model.Metric{Type: constants.GaugeType, Name: "StackInuse", Value: float64(rtMet.StackInuse)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "StackInuse", Value: float64(r.rtMet.StackInuse)}
 		case 24:
-			results <- model.Metric{Type: constants.GaugeType, Name: "StackSys", Value: float64(rtMet.StackSys)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "StackSys", Value: float64(r.rtMet.StackSys)}
 		case 25:
-			results <- model.Metric{Type: constants.GaugeType, Name: "Sys", Value: float64(rtMet.Sys)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "Sys", Value: float64(r.rtMet.Sys)}
 		case 26:
-			results <- model.Metric{Type: constants.GaugeType, Name: "TotalAlloc", Value: float64(rtMet.TotalAlloc)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "TotalAlloc", Value: float64(r.rtMet.TotalAlloc)}
 		case 27:
 			results <- model.Metric{Type: constants.GaugeType, Name: "RandomValue", Value: rand.Float64()}
 		case 28:
 			pCount++
 			results <- model.Metric{Type: constants.CounterType, Name: "PollCount", Value: pCount}
 		case 29:
-			results <- model.Metric{Type: constants.GaugeType, Name: "TotalMemory", Value: float64(vm.Total)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "TotalMemory", Value: float64(r.vm.Total)}
 		case 30:
-			results <- model.Metric{Type: constants.GaugeType, Name: "FreeMemory", Value: float64(vm.Free)}
+			results <- model.Metric{Type: constants.GaugeType, Name: "FreeMemory", Value: float64(r.vm.Free)}
 		case 31:
-			for i, c := range cpuUt {
+			for i, c := range r.cpuUt {
 				results <- model.Metric{Type: constants.GaugeType, Name: fmt.Sprintf("CPUutilization%v", i+1), Value: c}
 			}
 		}
